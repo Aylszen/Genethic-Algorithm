366b519e555315e31d7018ab5c9b2132234de242
Aylszen <aylszen@gmail.com>
2017-10-26 22:23:09 +0200

    mutateChromosomes() function added + small fixes

--- src/GenethicAlgorithm.java
+++ src/GenethicAlgorithm.java
@@ -8,30 +8,27 @@ public class GenethicAlgorithm {
 		List<Chromosome> nextGenChromosomeList = new ArrayList<Chromosome>();
 		final byte POPULATION_SIZE = 10;
 		final byte MAX_CHROMOSOME_VALUE = 127;
+		final byte PROBABILITY_OF_CROSSING = 75;
+		final byte PROBABILITY_OF_MUTATION = 2;
 
 		Population population = new Population();
 		population.chooseFirstPopulation(POPULATION_SIZE, MAX_CHROMOSOME_VALUE, chromosomeList);
-//#############################
-		for (int i = 0; i < 20; i++) {
-		population.assessmentOfAdaptation(POPULATION_SIZE, chromosomeList);
-		population.setProbabilityOfSelectionForEachChromosome(POPULATION_SIZE, chromosomeList);
-		nextGenChromosomeList = population.selectNextGeneration(POPULATION_SIZE, chromosomeList);
-
-		for (int j = 0; j < POPULATION_SIZE; j++) {
-			System.out.print(
-					chromosomeList.get(j).getPhenotype() + " | " + nextGenChromosomeList.get(j).getPhenotype() + "\n");
-			// System.out.print(firstPopulation.chromosomeList.get(i).getAdaptationLevel()
-			// + "\n");
-			// System.out.print(firstPopulation.chromosomeList.get(i).getProbabilityOfSelection()
-			// + "\n");
-		}
-		chromosomeList = nextGenChromosomeList;
-		if(chromosomeList == nextGenChromosomeList)
-		{
-			System.out.print("DUPA");
-		}
-		population.crossChromosomes(POPULATION_SIZE, chromosomeList);
-//#############################
+		// #############################
+		for (int i = 0; i < 2; i++) {
+			population.assessmentOfAdaptation(POPULATION_SIZE, chromosomeList);
+			population.setProbabilityOfSelectionForEachChromosome(POPULATION_SIZE, chromosomeList);
+			nextGenChromosomeList = population.selectNextGeneration(POPULATION_SIZE, chromosomeList);
+			for (int j = 0; j < POPULATION_SIZE; j++) {
+				System.out.print(chromosomeList.get(j).getPhenotype() + " | "
+						+ nextGenChromosomeList.get(j).getPhenotype() + "\n");
+				}
+			System.out.print("-----------------\n");
+			chromosomeList = nextGenChromosomeList;
+			
+			
+			population.crossChromosomes(POPULATION_SIZE, PROBABILITY_OF_CROSSING, chromosomeList);
+			population.mutateChromosomes(POPULATION_SIZE, PROBABILITY_OF_MUTATION, chromosomeList);
+			// #############################
 		}
 	}
 
--- src/Population.java
+++ src/Population.java
@@ -55,7 +55,7 @@ public class Population {
 		float probabilityOfSelectionSum = 0;
 		for (int j = 0; j < POPULATION_SIZE; j++) {
 			probabilityOfSelectionSum += chromosomeList.get(j).getProbabilityOfSelection();
-			System.out.printf("Random probab: %f probab of sel: %f\n", randomProbability, probabilityOfSelectionSum);
+			//System.out.printf("Random probab: %f probab of sel: %f\n", randomProbability, probabilityOfSelectionSum);
 			if (randomProbability < probabilityOfSelectionSum) {
 				nextGenChromosomeList.add(chromosomeList.get(j));
 				probabilityOfSelectionSum = 0;
@@ -64,17 +64,38 @@ public class Population {
 		}
 	}
 
-	public void crossChromosomes(final byte POPULATION_SIZE, List<Chromosome> chromosomeList) {
+	public void crossChromosomes(final byte POPULATION_SIZE, final byte PROBABILITY_OF_CROSSING,
+			List<Chromosome> chromosomeList) {
+		Random random = new Random();
+		if (random.nextInt(101) < PROBABILITY_OF_CROSSING) {
+			final byte LOCUS_SIZE = 7;
+			int locus = 0, ch1_locus = 0, ch2_locus = 0, ch1_at = 0, ch2_at = 0;
+			for (int i = 0; i < (POPULATION_SIZE / 2); i++) {
+				locus = random.nextInt(LOCUS_SIZE);
+				ch1_locus = chromosomeList.get(ch1_at = random.nextInt(POPULATION_SIZE)).getGenotype()[locus];
+				ch2_locus = chromosomeList.get(ch2_at = random.nextInt(POPULATION_SIZE)).getGenotype()[locus];
+				chromosomeList.get(ch1_at).getGenotype()[locus] = ch2_locus;
+				chromosomeList.get(ch2_at).getGenotype()[locus] = ch1_locus;
+			}
+		}
+
+	}
+
+	public void mutateChromosomes(final byte POPULATION_SIZE, final byte PROBABILITY_OF_MUTATION,
+			List<Chromosome> chromosomeList) {
 		Random random = new Random();
 		final byte LOCUS_SIZE = 7;
-		int locus, ch1_locus, ch2_locus, ch1_at, ch2_at = 0;
-		random.nextInt(POPULATION_SIZE);
-		for (int i = 0; i < (POPULATION_SIZE / 2); i++) {
-			locus = random.nextInt(LOCUS_SIZE);
-			ch1_locus = chromosomeList.get(ch1_at = random.nextInt(POPULATION_SIZE)).getGenotype()[locus];
-			ch2_locus = chromosomeList.get(ch2_at = random.nextInt(POPULATION_SIZE)).getGenotype()[locus];
-			chromosomeList.get(ch1_at).getGenotype()[locus] = ch2_locus;
-			chromosomeList.get(ch2_at).getGenotype()[locus] = ch1_locus;
+		int locus = 0;
+		for (int i = 0; i < POPULATION_SIZE; i++) {
+			if (random.nextInt(101) < PROBABILITY_OF_MUTATION) {
+				locus = random.nextInt(LOCUS_SIZE);
+				System.out.print("Uda³o siê");
+				if (chromosomeList.get(i).getGenotype()[locus] == 0) {
+					chromosomeList.get(i).getGenotype()[locus] = 1;
+				} else {
+					chromosomeList.get(i).getGenotype()[locus] = 0;
+				}
+			}
 		}
 	}
 
